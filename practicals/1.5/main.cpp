c89bcbcd8bb3d5fff6357916388dbc76360b1d21
stanislavbuket <9224853@stud.nau.edu.ua>
2024-09-20 10:31:33 +0300

    pract1.5

--- practicals/1.5/main.cpp
+++ practicals/1.5/main.cpp
@@ -1,13 +1,15 @@
 /**
  * Done by:
- * Student Name: Severyn Kotyhoroshko
- * Student Group: 123
+ * Student Name: Stanislav Buket
+ * Student Group: 121
  * Lab 1.5
  */
 
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -16,38 +18,89 @@ void initRandomizer() {
     srand(time(0));  // srand(time(NULL)) could also be used
 }
 
-int main() {
-    // A sequence of values A[n] is given. Find the minimum value in the sequence.
-    {
-        initRandomizer();
-
-        int n;
-        cout << "Enter the length of the sequence: ";
-        cin >> n;
-
-        std::vector<int> myVector;
-        // Populate the array with random values
-        for (int i = 0; i < n; i++) {
-            // arr[i] = rand();
-            int randomVar = (rand() % 100) - 50;
-            myVector.push_back(randomVar);
+// Function to find the index of the first occurrence of P in the sequence
+int findFirstOccurrence(const vector<int>& myVector, int P) {
+    for (int i = 0; i < myVector.size(); i++) {
+        if (myVector[i] == P) {
+            return i; // Returns the index of the first occurrence
         }
-
-        // cout the array
-        for (int i = 0; i < n; i++) {
-            cout << myVector[i] << " ";
-        }
-        cout << endl;
-
-        int min = myVector[0];
-        for (int i = 1; i < n; i++) {
-            if (myVector[i] < min) {
-                min = myVector[i];
-            }
-        }
-
-        cout << "The minimum value in the sequence is: " << min << endl;
     }
+    return -1; // If P is not found
+}
+
+// Function to find the smallest positive element in the sequence
+int findMinPositive(const vector<int>& myVector) {
+    int minPositive = -1;
+    for (int i = 0; i < myVector.size(); i++) {
+        if (myVector[i] > 0 && (minPositive == -1 || myVector[i] < minPositive)) {
+            minPositive = myVector[i];
+        }
+    }
+    return minPositive;
+}
+
+// Function to swap the largest and smallest elements in the sequence
+void swapMinMax(vector<int>& myVector) {
+    int minIndex = 0, maxIndex = 0;
+    for (int i = 1; i < myVector.size(); i++) {
+        if (myVector[i] < myVector[minIndex]) {
+            minIndex = i;
+        }
+        if (myVector[i] > myVector[maxIndex]) {
+            maxIndex = i;
+        }
+    }
+    swap(myVector[minIndex], myVector[maxIndex]);
+}
+
+int main() {
+    initRandomizer();
+
+    int n, P;
+    cout << "Enter the length of the sequence: ";
+    cin >> n;
+
+    vector<int> myVector;
+    // Populate the array with random values
+    for (int i = 0; i < n; i++) {
+        int randomVar = (rand() % 100) - 50;
+        myVector.push_back(randomVar);
+    }
+
+    // Output the array
+    for (int i = 0; i < n; i++) {
+        cout << myVector[i] << " ";
+    }
+    cout << endl;
+
+    // Task 1: Find the index of the first occurrence of P
+    cout << "Enter the value to find (P): ";
+    cin >> P;
+
+    int index = findFirstOccurrence(myVector, P);
+    if (index != -1) {
+        cout << "First occurrence of " << P << " is at index: " << index << endl;
+    }
+    else {
+        cout << P << " not found in the sequence." << endl;
+    }
+
+    // Task 2: Find the smallest positive element
+    int minPositive = findMinPositive(myVector);
+    if (minPositive != -1) {
+        cout << "The minimum positive value is: " << minPositive << endl;
+    }
+    else {
+        cout << "No positive values in the sequence." << endl;
+    }
+
+    // Task 3: Swap the largest and smallest elements
+    swapMinMax(myVector);
+    cout << "Sequence after swapping the min and max values: ";
+    for (int i = 0; i < n; i++) {
+        cout << myVector[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
