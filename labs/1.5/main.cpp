5b2e09bebbe92e30973518be8b1b4bdcc1b91108
stanislavbuket <9224853@stud.nau.edu.ua>
2024-09-20 13:55:26 +0300

    fix lab 1.5

--- labs/1.5/main.cpp
+++ labs/1.5/main.cpp
@@ -1,7 +1,7 @@
 /**
- * Done by:
+* Done by:
  * Student Name: Stanislav Buket
- * Variant: 4
+ * Vatiant: 4
  * Student Group: 121
  * Lab 1.5
  */
@@ -12,7 +12,7 @@ using namespace std;
 
 class Lab1_5 {
 public:
-    // Constructor
+    // Конструктор
     Lab1_5() {
         part1();
         part2();
@@ -20,40 +20,45 @@ public:
 
 private:
     void part1() {
-        // Set 1
+        // Набір 1
         int A1 = 66;
         double B1 = 12.9;
         int C1 = 62;
         int D1 = 59;
 
-        // Expression calculation for set 1
+        // Обчислення виразу для набору 1
         bool result1 = !(!(A1 != B1) && (C1 < D1));
-        cout << "Result for set 1: " << boolalpha << result1 << endl;
+        cout << "Результат для набору 1: " << boolalpha << result1 << endl;
 
-        // Set 2
+        // Набір 2
         double A2 = 3.56;
         double B2 = 89.45;
         int C2 = 15;
         double D2 = 56.2;
 
-        // Expression calculation for set 2
+        // Обчислення виразу для набору 2
         bool result2 = !(!(A2 != B2) && (C2 < D2));
-        cout << "Result for set 2: " << boolalpha << result2 << endl;
+        cout << "Результат для набору 2: " << boolalpha << result2 << endl;
     }
 
-    {
-        int a = 10;
-        int b = 20;
-        int c = 30;
-        int d = 40;
+    void part2() {
+        // Значення змінних для варіанту 4
+        int A = 593;
+        int B = -145;
+        float C = 9.23;
+        int D = 43;
+        int E = 891;
+        short F = sizeof(short);
 
-        bool res = !a;
-        cout << "res: " << boolalpha << res << endl;
+        // Вираз з використанням операцій, заданих у таблицях
+        int L_result = (((A ^ (B >> 1)) / C) * (-D) * sizeof(E)) < F;
+
+        cout << "Результат обчислення для Частини 2: " << L_result << endl;
     }
 };
 
 int main() {
-    // Creating an object of the class to execute both parts
+    // Створення об'єкта класу для виконання обох частин
     Lab1_5 lab;
     return 0;
 }
